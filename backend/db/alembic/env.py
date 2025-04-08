496816eeb2cd54688fe73576af568434a83ac294
yhonatan6273 <jony2082@gmail.com>
2025-04-08 18:50:08 +0300

    new

--- backend/db/alembic/env.py
+++ backend/db/alembic/env.py
@@ -2,7 +2,8 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+import os
+from dotenv import load_dotenv
 from alembic import context
 from backend.Models import models
 
