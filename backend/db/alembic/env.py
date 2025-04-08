51019717c883a09e5d684bcb668d0fe439e9c2a8
yhonatan6273 <jony2082@gmail.com>
2025-04-08 18:49:50 +0300

    new

--- backend/db/alembic/env.py
+++ backend/db/alembic/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 from backend.Models import models
+
+
+load_dotenv()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
