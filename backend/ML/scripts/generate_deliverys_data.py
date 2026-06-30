ee00b2d0e55e7dedde20fe6b1cdeeb2d7b1c5632
yhonatan6273 <jony2082@gmail.com>
2026-06-30 18:15:57 +0300

    feat: add K8s liveness probe and improve ML data generation logic

--- backend/ML/scripts/generate_deliverys_data.py
+++ backend/ML/scripts/generate_deliverys_data.py
@@ -8,21 +8,28 @@ import pandas as pd
 def make_random_deliverys(number_deliverys):
     deliverys_list=[]
     for _ in range(number_deliverys):
-        random_distance=np.random.uniform(low=0, high=60)
+        random_distance=np.random.uniform(low=1.0, high=60.0)
         random_hour=np.random.randint(low=0, high=24)
         random_day=np.random.randint(low=0, high=7)
+        #base time every km takes on average without traffic
+        base_rate = np.random.uniform(1.2, 1.8)
         #the time every km takes on average without traffic
-        base_eta=random_distance * 1.5
-        traffic_penalty = 0
+        base_eta=random_distance * base_rate
+        
+        traffic_multiplier = 1.0
         #morning traffic penalty
         if 8<random_hour<11:
-                traffic_penalty = 15
-            #afternoon traffic penalty
+                traffic_multiplier = np.random.uniform(1.2, 2.5)
+        #afternoon traffic penalty
         elif 16<random_hour<19:
-                traffic_penalty = 30
-        #the random noise to simulate real life variations
-        random_noise = np.random.uniform(-5, 5)
-        final_eta = base_eta + traffic_penalty + random_noise
+                traffic_multiplier = np.random.uniform(1.4, 2.8)
+        else:
+                traffic_multiplier = np.random.uniform(0.9, 1.3)
+        
+        random_noise = np.random.uniform(-5.0, 12.0)
+        final_eta = (base_eta * traffic_multiplier) + random_noise
+        #ensure that the final eta is not negative and has a minimum of 5 minutes
+        final_eta = max(4.0, final_eta)
 
         delivery_information = {
             "distance_km": round(random_distance,2),
